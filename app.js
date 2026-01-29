9ab3ca19150d99ccd175c46b0afc3be0c39cdfa2
sdfasdfekkbpijsfvoias <tmdghks7388@gmail.com>
2026-01-30 03:24:57 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
-
-app.get('/', (req, res) => res.send('Koyeb 서버 생존 확인!'));
-
 app.post('/skill', (req, res) => {
+  // 카카오가 보내주는 데이터에서 '어떤 블록에서 왔는지' 이름을 읽습니다.
   const blockName = req.body.intent.name;
-  let message = "서버 연결 성공! 현재 블록: " + blockName;
+  let message = "무엇을 도와드릴까요?";
 
-  if (blockName === "태그 달기") {
-    message = "반가워요 사장님! '태그 달기' 연결 완료됐습니다. ✨";
+  // 사장님이 요청하신 시나리오별 분기 처리
+  if (blockName === "시나리오1") {
+    message = "어서오세요";
+  } else if (blockName === "시나리오2") {
+    message = "반가워요";
   }
 
   res.json({
     "version": "2.0",
-    "template": { "outputs": [{ "simpleText": { "text": message } }] }
+    "template": {
+      "outputs": [{
+        "simpleText": {
+          "text": message
+        }
+      }]
+    }
   });
 });
-
-app.listen(process.env.PORT || 8000);
